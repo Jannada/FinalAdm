29903fe5fa39a390c85e902bfecfa6c80010b334
Jannada <vivianabeltre0912@gmail.com>
2019-03-06 15:03:25 -0400

    Usuarios Tabla

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,11 +1,13 @@
 <?php
 class Conexion{
+
     static public function conectar(){
         
-        $link = new PDO("mysql:host=localhost;dbname=financiera", "root", "");
+        $link = new PDO("mysql:host=localhost;dbname=financiera",
+        "root",
+        "");
 
-        $link = exec("set names utf-8");
+        $link -> exec("set names utf8");
         return $link;
     }
 }
-?>
