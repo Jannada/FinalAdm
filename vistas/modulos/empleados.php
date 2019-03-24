9d79ade47900f659abfa645f9daa03ffe1753ffd
Deivid Castillo <deividC047@gmail.com>
2019-03-23 23:26:16 -0400

    DEIVID

--- vistas/modulos/empleados.php
+++ vistas/modulos/empleados.php
@@ -57,13 +57,15 @@
                     <td>
                     <div class="btn-group">
                           
-                    <button class="btn btn-warning btnEditarEmpleado" data-toggle="modal" data-target="#modalEditarEmpleado" idCliente="'.$value["id"].'"><i class="fa fa-pencil"></i></button>';
+                    <button class="btn btn-warning btnEditarCliente" data-toggle="modal" data-target="#modalEditarEmpleado" idCliente="'.$value["id"].'"><i class="fa fa-pencil"></i></button>';
 
-                      if($_SESSION["perfil"] == "Administrador"){
+                    if($_SESSION["perfil"] == "Administrador"){
 
-                      echo '<button class="btn btn-danger btnEliminarEmpleado" idEmpleado="'.$value["id"].'"><i class="fa fa-times"></i></button>';
+                        echo '<button class="btn btn-danger btnEliminarEmpleado" idCliente="'.$value["id"].'"><i class="fa fa-times"></i></button>';
 
-                  }
+                    }
+
+                    echo 
 
                   '</div>  
                 </tr>';
@@ -203,4 +205,163 @@
     </div>
   </div>
 </div>
+
+<!--=====================================
+MODAL EDITAR Empleado
+======================================-->
+
+<div id="modalEditarEmpleado" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar Empleado</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarCliente" id="editarCliente" required>
+                <input type="hidden" id="idCliente" name="idCliente">
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL DOCUMENTO ID -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input type="number" min="0" class="form-control input-lg" name="editarDocumentoId" id="editarDocumentoId" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL EMAIL -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span> 
+
+                <input type="email" class="form-control input-lg" name="editarEmail" id="editarEmail" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL TELÉFONO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarTelefono" id="editarTelefono" data-inputmask="'mask':'(999) 999-9999'" data-mask required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA DIRECCIÓN -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-map-marker"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarDireccion" id="editarDireccion"  required>
+
+              </div>
+
+            </div>
+
+             <!-- ENTRADA PARA LA FECHA DE NACIMIENTO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarFechaNacimiento" id="editarFechaNacimiento"  data-inputmask="'alias': 'yyyy/mm/dd'" data-mask required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+
+        </div>
+
+      </form>
+
+      <?php
+
+        $editarCliente = new ControladorEmpleados();
+        $editarCliente -> ctrEditarEmpleado();
+
+      ?>
+
+    
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php
+
+  $eliminarCliente = new ControladorEmpleados();
+  $eliminarCliente -> ctrEliminarEmpleado();
+
+?>
+
+
+
 
