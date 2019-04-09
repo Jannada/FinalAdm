010745f00646e5d93f81a13a715312d1c98e2775
Finalandr <20163524@itla.edu.do>
2019-04-09 18:12:26 -0400

    prestamos

--- vistas/js/crear-prestamos.js
+++ vistas/js/crear-prestamos.js
@@ -6,3 +6,38 @@ $(document).on("change", "#gridCheck", function(){
         $(".contenido-garante").hide();
     }
 })
+
+$(document).on("change", "#plazoPrestamo", function(){
+    
+   var monto = parseInt($("#montoPrestamo").val());
+   var plazo = $("#plazoPrestamo").val();
+
+   var porcentaje = (8/100)/12;
+
+   var interes = parseInt((monto*porcentaje)*plazo);
+   //var interesMen= interes/plazo;
+   //var capital= monto/plazo;
+   var cuotas =(monto/plazo)+(interes/plazo);
+   var total = monto+interes;
+
+   $("#interesPrestamo").val(interes);
+   $("#cuotasMensuales").val(cuotas);
+
+   console.log(monto);
+   console.log(interes);
+   console.log(total);
+
+    for(var i=0; i<plazo;i++){
+
+        var interesMen= monto*porcentaje;
+        var capital = cuotas-interesMen;
+
+        console.log(interesMen);
+        console.log(capital);
+        console.log(cuotas);
+
+        monto -= cuotas;
+    }
+   
+})
+
