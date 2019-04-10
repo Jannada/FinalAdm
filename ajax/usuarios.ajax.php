4ad2f9ba2486427120cd0afb9ca0981b175e6080
Finalandr <20163524@itla.edu.do>
2019-04-10 12:19:18 -0400

    merge exitoso

--- ajax/usuarios.ajax.php
+++ ajax/usuarios.ajax.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 require_once "../controladores/usuarios.controlador.php";
 require_once "../modelos/usuarios.modelo.php";
 require_once "../controladores/empleados.controlador.php";
