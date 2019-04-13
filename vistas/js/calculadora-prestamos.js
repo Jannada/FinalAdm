280e8059d3f1c14b2e6271508125b37d23fe442f
Finalandr <20163524@itla.edu.do>
2019-04-12 21:09:12 -0400

    calc

--- vistas/js/calculadora-prestamos.js
+++ vistas/js/calculadora-prestamos.js
@@ -12,11 +12,11 @@
 
    var porcentaje = (tasa/100)/12;
 
-   var interes = parseInt((monto*porcentaje)*plazo);
+   var interes = parseInt((monto*porcentaje));
    //var interesMen= interes/plazo;
    //var capital= monto/plazo;
    var cuotas =(monto/plazo)+(interes/plazo);
-   var total = monto+interes;
+   var total = cuotas-interes;
 
 //    $("#interesPrestamo").val(interes);
 //    $("#cuotasMensuales").val(cuotas);
@@ -37,26 +37,32 @@
 
    var fecha1 = new Date(suma);
 
-    // var fechaFin = (fecha1.getFullYear()+"/"+(fecha1.getMonth()+1)+"/"+fecha1.getDate());
+   var mes = 1000 * 60 * 60 * 24 * 30 * plazo;
 
-    // $("#fechaFinPago").val(fechaFin);
+   var suma2 = fecha1.getTime() + mes;
+   var fecha2 = new Date(suma2);
+
+    var fechaFin = (fecha2.getFullYear()+"/"+(fecha2.getMonth()+1)+"/"+fecha2.getDate());
+
+    $("#fechaFinPago").val(fechaFin);
+
 
     for(var i=0; i<plazo;i++){
 
-        var mes = 1000 * 60 * 60 * 24 * 30 * i;
-        var suma2 = fecha.getTime() + mes;
-        var fecha2 = new Date(suma2);
+        
+        // var suma2 = fecha.getTime() + mes;
+        // var fecha2 = new Date(suma2);
 
-        var interesMen= monto*porcentaje;
-        var capital = cuotas-interesMen;
-        var fechaFin = (fecha2.getFullYear()+"/"+(fecha2.getMonth()+1)+"/"+fecha2.getDate());
+        // var interesMen= monto*porcentaje;
+        // var capital = cuotas-interesMen;
+        // var fechaFin = (fecha2.getFullYear()+"/"+(fecha2.getMonth()+1)+"/"+fecha2.getDate());
 
 
-        console.log(monto);
-        console.log(interesMen);
-        console.log(capital);
-        console.log(cuotas);
-        console.log(fecha2.getFullYear()+"/"+(fecha2.getMonth()+1)+"/"+fecha.getDate());
+        // console.log(monto);
+        // console.log(interesMen);
+        // console.log(capital);
+        // console.log(cuotas);
+        // console.log(fecha2.getFullYear()+"/"+(fecha2.getMonth()+1)+"/"+fecha.getDate());
 
         // monto -= cuotas;
         
@@ -69,7 +75,7 @@
             '<td>'+fechaFin+'</td>'+
             // '<td>'+Math.round(monto*100)/100+'</td>'+
             '<td>'+Math.round(interes*100)/100+'</td>'+
-            '<td>'+Math.round(capital*100)/100+'</td>'+
+            '<td>'+Math.round(total*100)/100+'</td>'+
             '<td>'+Math.round(cuotas*100)/100+'</td>'+
         '</tr>'
 
